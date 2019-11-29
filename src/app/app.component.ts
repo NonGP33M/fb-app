a9974f23d14073414e2f7498cebb37e0e08d85f7
NonGP33M <phumrapee.sk@gmail.com>
2019-11-29 15:32:54 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fb-app';
+  textMessage = " "
+  doIt(){
+    console.log(this.textMessage)
+  }
 }
